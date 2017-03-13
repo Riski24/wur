d058087eaa40d7cc0730212d4dc481fccbd5f327
Kyle Anson <kyle.anson7@gmail.com>
2017-03-13 18:35:53 -0400

    NOW "Hello World" should be displaying

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
-require('./routes.js');
+require('./routes.js')(app);
 
-//module.exports = app;+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log('Listening on port:', port);
+
+//module.exports = app;
