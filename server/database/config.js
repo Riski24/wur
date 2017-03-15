93507e55c0b110d10579c0a834687d043f6f58df
Kyle Anson <kyle.anson7@gmail.com>
2017-03-14 22:23:02 -0400

    Getting ready for users database implementation

--- server/database/config.js
+++ server/database/config.js
@@ -1,14 +1,19 @@
 var Sequelize = require('sequelize');
 var db = null;
 if (process.env.DATABASE_URL) {
-  db = new Sequelize(process.env.DATABASE_URL, {
-    logging: false
-  });
+  db = new Sequelize(process.env.DATABASE_URL);
 } else {
   db = new Sequelize('wur_local', 'root', '', {
     logging: false
   });
 }
+
+var users = db.define('users', {
+  username: {type: Sequelize.STRING, allowNull: false, unique: true}
+  //password: {type: Sequelize.STRING, allowNull: false}
+}, {
+  timestamps: false
+});
 
 var questions = db.define('questions', {
   choice1: {type: Sequelize.STRING, allowNull: false},
@@ -19,7 +24,43 @@ var questions = db.define('questions', {
   timestamps: false
 });
 
+var userQuestionVotes = db.define('user_question_votes', {
+  userid: {type: Sequelize.INTEGER, allowNull: false},
+  questionid: {type: Sequelize.INTEGER, allowNull: false},
+  choice: {type: Sequelize.INTEGER, allowNull: false}
+}, {
+  timestamps: false
+});
 
-questions.sync();
+// users.belongsToMany(questions, {through: 'user_question_vote'});
+// questions.belongsToMany(users, {through: 'user_question_vote'});
 
+users.sync();
+questions.sync({force: true})
+.then(function() {
+/////////////////////////////////////////////////////////////////
+////////////////////////// SAMPLE DATA //////////////////////////
+  var sampleData = [
+    {choice1: 'Always be late', choice2: 'Always be unprepared'},
+    {choice1: 'Be a carrot', choice2: 'Be a cucumber'},
+    {choice1: 'Always be naked', choice2: 'Always be itchy'},
+    {choice1: 'Have no thumbs', choice2: 'Have only thumbs'},
+    {choice1: 'Never read another book', choice2: 'Never hear another song'}
+  ];
+
+  for (var i = 0; i < sampleData.length; i++) {
+    (function(i) {
+      questions.create({
+        choice1: sampleData[i].choice1,
+        choice2: sampleData[i].choice2
+      });
+    })(i)
+  }
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+});
+userQuestionVotes.sync();
+
+exports.users = users;
 exports.questions = questions;
+exports.userQuestionVotes = userQuestionVotes;
